a2cc76df41b78537a26a901576782ecf8eb812a0
root <root@DESKTOP-4SRDOID.localdomain>
2024-06-11 17:25:29 +0300

    will be fixed

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""This module has the code"""
+
+
+def minOperations(n):
+    """This is what was missing"""
+    if n <= 1:
+        return 0
+    operations = 0
+    factor = 2
+    while n > 1:
+        while n % factor == 0:
+            operations += factor
+            n //= factor
+        factor += 1
+    return operations
