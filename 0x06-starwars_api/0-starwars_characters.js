87b94229c6d0b69c892e66bbc94b1ad1bd98a43d
Omnia Ahmed <139129370+Moniaar@users.noreply.github.com>
2024-07-15 05:14:39 +0300

    Update 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,43 +1,20 @@
 #!/usr/bin/node
 
 const request = require('request');
+const util = require('util');
+const filmId = process.argv[2];
+const URL = `https://swapi-api.alx-tools.com/api/films/${filmId}`;
 
-if (process.argv.length < 3) {
-    console.error('Usage: node 0-starwars_characters.js <Movie ID>');
-    process.exit(1);
-}
+const prequest = util.promisify(request);
 
-const movieID = process.argv[2];
-const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieID}/`;
-
-request(apiUrl, (error, response, body) => {
-    if (error) {
-        console.error('Error:', error);
-        return;
+(async () => {
+  try {
+    const film = (await prequest(URL, { json: true })).body;
+    for (const url of film.characters) {
+      const character = (await prequest(url, { json: true })).body;
+      console.log(character.name);
     }
-
-    if (response.statusCode !== 200) {
-        console.error('Failed to retrieve movie:', response.statusCode);
-        return;
-    }
-
-    const movie = JSON.parse(body);
-    const characterUrls = movie.characters;
-
-    characterUrls.forEach((url) => {
-        request(url, (charError, charResponse, charBody) => {
-            if (charError) {
-                console.error('Error:', charError);
-                return;
-            }
-
-            if (charResponse.statusCode !== 200) {
-                console.error('Failed to retrieve character:', charResponse.statusCode);
-                return;
-            }
-
-            const character = JSON.parse(charBody);
-            console.log(character.name);
-        });
-    });
-});
+  } catch (error) {
+    console.log(error);
+  }
+})();
